a1d7acf27eaa6bd85c0d0f055295efdd3ef061a6
highapptitude <wayne.brandon@gmail.com>
2023-04-26 03:54:05 +0000

    code challenge

---
+++ helloworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script Name:                  Helloworld
+# Author:                       Wayne Brandon
+# Date of latest revision:      04/25/2023
+# Purpose:                      learning
+
+# Declaration of variables
+baseball="I am working perfectly!"
+
+# Declaration of functions
+
+
+# Main
+echo "Hello World"
+echo $baseball
+
+# End
