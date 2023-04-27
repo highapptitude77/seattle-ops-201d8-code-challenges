1840d85c7780dfe64720c1c9c18a803e42c52399
highapptitude <wayne.brandon@gmail.com>
2023-04-27 01:33:45 +0000

    this is fun

---
+++ loginhistory.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Script Name:                  Ops challenge 03
+# Author:                       Big-Daddy
+# Date of latest revision:      04/26/2023
+# Purpose:                      Educational (functions in bash)
+
+# Declaration of variables
+
+# Declaration of functions
+
+print_something () {
+  last 
+  echo “This is the login history”.
+
+}
+
+
+# Main
+print_something
+print_something
+print_something
+
+# End
