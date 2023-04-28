f261bc6ff4cd2e30b563ff62abf2afbd61121bc4
highapptitude <wayne.brandon@gmail.com>
2023-04-28 03:22:06 +0000

    arrays-lab

---
+++ arrays.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge 04
+# Author:                       Wayne Brandon
+# Date of latest revision:      04/27/2023
+# Purpose:                      Educational (Arrays in Bash)
+
+# Declaration of variables
+# Create four directories: dir1, dir2, dir3, dir4
+dir1="exercise_dir"
+dir2="diet_dir"
+dir3="boxing_dir"
+dir4="improvement_dir"
+
+file1="exercise.txt"
+file2="diet.txt"
+file3="boxing.txt"
+file4="improvement.txt"
+
+
+
+# array
+directory_array=($dir1 $dir2 $dir3 $dir4)
+
+# Declaration of functions
+
+
+
+
+
+# Main
+
+
+# names of the directories in the array
+mkdir $dir1
+mkdir $dir2
+mkdir $dir3
+mkdir $dir4
+
+
+# END
